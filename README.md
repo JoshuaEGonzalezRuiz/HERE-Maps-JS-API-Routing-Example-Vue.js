791ed2cd91b81f38009077e28356fcf9de32895e
Joshua <jveiki3@gmail.com>
2023-02-06 23:27:27 -0600

    Project files are added

---
+++ README.md
@@ -0,0 +1,10 @@
+# HERE Maps JS API - Routing Example (Vue.js)
+
+This project exemplifies how to use the HERE Maps JS API together with Vue.js to perform a route calculation.
+
+- HERE Maps JS API: https://developer.here.com/documentation/maps/dev_guide/index.html
+  
+- Vue.js: https://vuejs.org/
+
+
+##### Developed by: Joshua González
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <HereMap  :center="center" />
+  </div>
+</template>
+
+<script>
+import HereMap from './components/HereMap'
+export default {
+  name: 'app',
+  components: {
+    HereMap
+    // Remove the HelloWorld.vue 
+  },
+  data() {
+return {
+    // we are this as prop to the HereMap component 
+  center:{ 
+    lat: 40.730610, 
+    lng: -73.935242
+    }
+}
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
